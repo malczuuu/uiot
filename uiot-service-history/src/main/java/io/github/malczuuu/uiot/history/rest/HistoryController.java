738fe95323dadd9832ff578a36bca0286e7f8ad1
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-10-04 18:54:42 +0200

    Restore docker-build-all.yml

--- uiot-service-history/src/main/java/io/github/malczuuu/uiot/history/rest/HistoryController.java
+++ uiot-service-history/src/main/java/io/github/malczuuu/uiot/history/rest/HistoryController.java
@@ -2,7 +2,6 @@ package io.github.malczuuu.uiot.history.rest;
 
 import io.github.malczuuu.uiot.history.core.HistoryService;
 import io.github.malczuuu.uiot.history.model.EventHistory;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import org.springframework.http.MediaType;
@@ -24,16 +23,17 @@ public class HistoryController {
 
   @Parameters({@Parameter(name = "thing_uid")})
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  public EventHistory getHistory(@PathVariable("room") String roomUid) {
+  public EventHistory getHistory(
+      @PathVariable("room") String roomUid, @RequestParam("thing_uid") String thingUid) {
     return historyService.getEventHistory(roomUid, 100);
   }
-
-  @Operation(hidden = true)
-  @GetMapping(
-      params = {"thing_uid"},
-      produces = MediaType.APPLICATION_JSON_VALUE)
-  public EventHistory getThingHistory(
-      @PathVariable("room") String roomUid, @RequestParam("thing_uid") String thingUid) {
-    return historyService.getThingEventHistory(roomUid, thingUid, 100);
-  }
+  //
+  //  @Operation(hidden = true)
+  //  @GetMapping(
+  //      params = {"thing_uid"},
+  //      produces = MediaType.APPLICATION_JSON_VALUE)
+  //  public EventHistory getThingHistory(
+  //      @PathVariable("room") String roomUid, @RequestParam("thing_uid") String thingUid) {
+  //    return historyService.getThingEventHistory(roomUid, thingUid, 100);
+  //  }
 }
